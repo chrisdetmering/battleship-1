ae67f3edf269a8f57b91f34602a26cef6c684e57
VirtualVasquez <mvasq95@hotmail.com>
2021-05-30 19:01:02 -0400

    progressed to 'front-end' of game, no logic written

--- Battleship/Program.cs
+++ Battleship/Program.cs
@@ -6,10 +6,15 @@ namespace Battleship
     {
         static void Main(string[] args)
         {
-            Intro();
+            /*Intro();
             Console.Clear();
             Console.WriteLine("Do you want a tutorial? (y or n)");
-            showTutorial(Console.ReadLine());
+            showTutorial(Console.ReadLine());*/
+            //Game();
+            var battleShip = new int[5, 2]{
+                { 5, 5 }, { 5,6}, { 5,7}, { 5,8}, { 5,9}
+            };
+            Console.WriteLine(battleShip[1, 1]);
         }
         static void Intro()
         {
@@ -29,21 +34,17 @@ namespace Battleship
             if (answer == "y")
             {
                 Console.Clear();
-                Console.WriteLine("*");
-                for (int i = 0; i < 49; i++)
+                for (int i = 0; i < 50; i++)
                 {
                     Console.Write("*");
                 }
+                Console.WriteLine("");
                 Console.WriteLine("The Game is simple, you will be prompted to select a point ona  10 x 10 grid.");
                 Console.WriteLine("The point is made up of X-value and a Y-value that represent on square of the grid ... Please see below");
-                for (int i = 0; i < 10; i++)
-                {
-                    Console.WriteLine("{0} - - - - - - - - - -", i);
-                }
-                Console.WriteLine("0 1 2 3 4 5 6 7 8 9 10");
+                GenerateGrid();
                 Console.WriteLine("Once the point is selected you will be prompted with a hit or miss.");
                 Console.WriteLine("You will be given a total of 8 guesses. If you do not destroy the nemy vessel...");
-                Console.WriteLine("You are probably a fit for the 1588 spanish armada and lsoe the game.");
+                Console.WriteLine("You are probably a fit for the 1588 spanish armada and lose the game.");
                 Console.WriteLine("However if you annihalate the enemy which takes up 5 grid spaces you win!");
                 Console.WriteLine("After which you can gloat to your friends and family and garner the respect of your peers");
                 for (int i = 0; i < 50; i++)
@@ -53,6 +54,32 @@ namespace Battleship
 
             }
         }
+        static void Game()
+        {
+            int Remaining = 8;
+            int Hits = 0;
+            int Misses = 0;
+            int inputX = 0;
+            int inputY = 0;
 
+            Console.WriteLine("Shots Remaining = {0} | Hits = {1} | Misses = {2}", Remaining, Hits, Misses);
+
+            GenerateGrid();
+            Console.WriteLine("");
+
+            Console.Write("(X-Axis) - Select a spot [1-10] to fire upon : ");
+            inputX = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.Write("(Y-Axis) - Select a spot [1-10] to fire upon : ");
+            inputY = int.Parse(Console.ReadLine());
+        }
+        static void GenerateGrid()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("{0} - - - - - - - - - -", i);
+            }
+            Console.WriteLine("0 1 2 3 4 5 6 7 8 9 10");
+        }
     }
 }
